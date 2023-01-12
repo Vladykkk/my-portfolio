640b9977fcb9ca1aa625697db7a2c27a12d87c83
Vladyslav <41453669+Vladykkk@users.noreply.github.com>
2023-01-12 15:55:11 +0200

    made better structure and add contact form

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-// Variables
-const header = document.querySelector(".header");
+"use strict"
 
-let burger = document.querySelector(".header__burger");
-let menu = document.querySelector(".header__menu");
-let list = document.querySelector(".header__list");
-let logo = document.querySelector(".header__logo")
-let moveLock = document.querySelector("body");
-let lastScrollY = window.scrollY;
+const header = document.querySelector(".header");
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
+const list = document.querySelector(".header__list");
+const logo = document.querySelector(".header__logo")
+const moveLock = document.querySelector(".body");
+
+const lastScrollY = window.scrollY;
 
 // Close menu on icon
 burger.onclick = function () {
@@ -29,3 +30,80 @@ document.querySelectorAll(".header__logo, .header__link").forEach(n => n.addEven
 	burger.classList.remove("active");
 	menu.classList.remove("active");
 }))
+
+// Form
+
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("form");
+
+	form.addEventListener("submit", formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		if (error === 0) {
+			form.classList.add("_sending");
+			let response = await fetch("sendmail.php", {
+				method: "POST",
+				body: formData
+			});
+
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				formPreview.innerHTML = "";
+				form.reset();
+				form.classList.remove("_sending");
+			} else {
+				alert("Error");
+				form.classList.remove("_sending");
+			}
+
+		} else {
+			alert("Fill in the required fields")
+		}
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll("._req");
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains("_email")) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+				formAddError(input);
+				error++;
+			} else {
+				if (input.value === "") {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add("_error");
+		input.classList.add("_error");
+	}
+
+	function formRemoveError(input) {
+		input.parentElement.classList.remove("_error");
+		input.classList.remove("_error");
+	}
+
+	// Функція тесту email
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+});
